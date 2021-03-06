44fc18b87f757e547476116f0786531e254715aa
cyberkillua <moshood988@gmail.com>
2021-03-06 17:33:29 +0100

    fixed time and added a border

--- src/Component/displayTable.js
+++ src/Component/displayTable.js
@@ -2,30 +2,25 @@ import React from "react";
 import styled from "styled-components";
 
 const Display = styled.section`
-  position: absolute;
-  width: 799px;
   height: 38px;
-  left: 331px;
-  top: 500px;
   font-family: Poppins;
   font-style: normal;
-
   line-height: 37px;
   color: #3b3d2d;
+  text-align: center;
+  margin-top: 100px;
   @media (max-width: 768px) {
     height: 15px;
-    width: 320px;
-    left: 22px;
-    top: 400px;
     border-radius: nullpx;
     font-size: 10px;
     line-height: 15px;
   }
   table {
-    border: 0px;
-
+    margin-left: auto;
+    margin-right: auto;
     td {
       padding: 10px;
+      border: 2px #94aa0f solid;
     }
   }
 `;
@@ -49,30 +44,30 @@ const Displaytable = ({ data }) => {
               <h2>Registrant Contact Information:</h2>
               <table>
                 <tbody>
-                <tr>
-                  <td> Name</td>
-                  <td> {body.registrant_name}</td>
-                </tr>
-                <tr>
-                  <td>Organization</td>
-                  <td>{body.registrant_org}</td>
-                </tr>
-                <tr>
-                  <td>Address</td>
-                  <td>{body.registrant_address}</td>
-                </tr>
-                <tr>
-                  <td>Expires On</td>
-                  <td>{body.expiry_date}</td>
-                </tr>
-                <tr>
-                  <td>Registered On</td>
-                  <td>{body.registration_date}</td>
-                </tr>
-                <tr>
-                  <td>Updated On</td>
-                  <td>{body.updated_date}</td>
-                </tr>
+                  <tr>
+                    <td> Name</td>
+                    <td> {body.registrant_name}</td>
+                  </tr>
+                  <tr>
+                    <td>Organization</td>
+                    <td>{body.registrant_org}</td>
+                  </tr>
+                  <tr>
+                    <td>Address</td>
+                    <td>{body.registrant_address}</td>
+                  </tr>
+                  <tr>
+                    <td>Expires On</td>
+                    <td>{body.expiry_date}</td>
+                  </tr>
+                  <tr>
+                    <td>Registered On</td>
+                    <td>{new Date(body.registration_date).toDateString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Updated On</td>
+                    <td>{new Date(body.updated_date).toDateString()}</td>
+                  </tr>
                 </tbody>
               </table>
             </>
